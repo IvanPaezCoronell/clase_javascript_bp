7974a0fb6ed73d29e0a7af8ed46259a3b0e47169
Ivan Paez Coronell <ipaez1@cuc.edu.co>
2021-08-31 19:43:03 -0500

    Hola mundo, Variables y  Constantes

---
+++ script.js
@@ -0,0 +1,33 @@
+// alerta
+// alert('Hello World')
+
+// VARIABLES
+mensaje = "Hello World";
+alert(mensaje);
+
+let a= 3, b=4, c=5;
+console.log(a+b);
+
+let nombre = "Ivan"
+let apellido = "Paez"
+
+console.log("Hola",nombre,apellido);
+
+// NOMBRAR VARIABLES
+
+// camel case
+let nombreUno = "Ivan"
+
+// underscore
+nombre_uno = "Ivan"
+
+// NOMBRES NO COMUNES
+let $ = 1;
+let _ = 2;
+console.log($,_);
+
+
+// CONSTANTES   
+const PI = 3.14;
+const COLOR_NEGRO = "#FFFFF";
+
